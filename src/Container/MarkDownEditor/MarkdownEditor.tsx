8440f960947a3c4b5c5c0575b9036667241e04bf
pidtchay <pidtchay@gmail.com>
2020-07-05 02:11:00 +0300

    Added module with markdown text conversion

---
+++ src/Container/MarkDownEditor/MarkdownEditor.tsx
@@ -0,0 +1,33 @@
+import { Row, Typography } from 'antd';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'Store';
+import { get } from 'Utils/common';
+import EditorContext from './Context';
+import { MarkdownInput } from './MarkDownInput';
+import { MarkdownResult } from './MarkdownResult';
+
+const { Title } = Typography;
+
+export const MarkdownEditor = () => {
+    const [markdownText, setMarkdownText] = useState('');
+    const literals = useSelector((state: RootState) => state.literals);
+
+    const contextValue = {
+        markdownText,
+        setMarkdownText: (value: string) => setMarkdownText(value)
+    };
+    return (
+        <EditorContext.Provider value={contextValue}>
+            <Row>
+                <Title level={1}>
+                    {get(literals, 'Pages.Markdown.page_title')}
+                </Title>
+            </Row>
+            <Row>
+                <MarkdownInput literals={literals} />
+                <MarkdownResult literals={literals} />
+            </Row>
+        </EditorContext.Provider>
+    );
+};
